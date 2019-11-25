14ec8e8dc6da0d8dcb11f6b6e04debbdaadabcb4
LEFEBVRE Benoit <benlef99@gmail.com>
2019-11-25 14:14:25 +0100

    ajout de contHelp + ajout des dépendances dans compilation.sh

--- WEB-INF/compilation.sh
+++ WEB-INF/compilation.sh
@@ -5,7 +5,7 @@ cd ../../../bin
 cd ../webapps/projetprogweb/WEB-INF/classes
 rm *.class
 cd ../lib
-javac *.java
+javac -cp ".:lib/commons-io-2.6.jar:lib/postgresql-jdbc.jar:lib/servlet-api.jar:lib/unbescape-1.1.6.RELEASE.jar" *.java
 mv *.class ../classes
 cd ../../../../bin
 ./startup.sh
